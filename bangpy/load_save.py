5f8cbd3c853ba2bf33b7a9e55d0bdf07b071124c
Zac Johnston <zac.johnston@monash.edu>
2019-10-17 15:31:23 -0400

    [load_save.py] added load_dat()

--- bangpy/load_save.py
+++ bangpy/load_save.py
@@ -1,6 +1,7 @@
 """Functions for loading/saving files
 """
 import os
+import numpy as np
 import configparser
 import ast
 
@@ -25,3 +26,21 @@ def load_config(filepath, verbose=True):
             config[section][option] = ast.literal_eval(ini.get(section, option))
 
     return config
+
+
+def load_dat(filepath, cols_dict):
+    """Loads .dat file and returns as dict of quantities
+
+    parameters
+    ----------
+    filepath: str
+    cols_dict : {}
+        dictionary with column names and indexes (Note: 1-indexed)
+    """
+    dat = {}
+    for key, idx_1 in cols_dict.items():
+        print(idx_1)
+        index = idx_1 - 1
+        dat[key] = np.loadtxt(filepath, usecols=index)
+
+    return dat
