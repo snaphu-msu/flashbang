26845fb873129991ba13efc34360dc2b18862493
Zac Johnston <zac.johnston@monash.edu>
2019-10-17 15:01:19 -0400

    [paths.py] added dat_filename() and dat_filepath()

--- bangpy/paths.py
+++ bangpy/paths.py
@@ -4,12 +4,39 @@ Naming convention:
   - "_filename" name of file only
   - "_path" full path to a directory
   - "_filepath" full path to a file (i.e., path + filename)
+
+Expected directory structure:
+[TODO]
 """
 
 import os
 
+# Bash environment variables, need to set these first
 bangpy_path = os.environ['BANGPY']
 models_path = os.environ['BANG_MODELS']
+
+
+def dat_filename(job_name):
+    """Returns filename for .dat file
+    """
+    return f'{job_name}.dat'
+
+
+def dat_filepath(job_name, model, runs_path=None, runs_prefix='run_'):
+    """Returns filepath to .dat file
+
+    parameters
+    ----------
+    job_name : str
+    model : str
+    runs_path : str
+        see model_path()
+    runs_prefix : str
+        see model_path()
+    """
+    filename = dat_filename(job_name)
+    path = model_path(model, runs_path=runs_path, runs_prefix=runs_prefix)
+    return os.path.join(path, filename)
 
 
 def config_filepath(name='default'):
@@ -24,7 +51,7 @@ def config_filepath(name='default'):
     return os.path.join(bangpy_path, 'bangpy', 'config', f'{name}.ini')
 
 
-def model_path(model, runs_path=None, prefix='run_'):
+def model_path(model, runs_path=None, runs_prefix='run_'):
     """Returns path to model directory
 
     parameters
@@ -34,11 +61,11 @@ def model_path(model, runs_path=None, prefix='run_'):
     runs_path : str (optional)
         path to directory containing all flash models
         defaults to environment variable BANG_MODELS
-    prefix : str (optional)
+    runs_prefix : str (optional)
          prefix of model directory
          defaults to 'run_'
     """
     if runs_path is None:
         runs_path = models_path
 
-    return os.path.join(runs_path, f'{prefix}{model}')
+    return os.path.join(runs_path, f'{runs_prefix}{model}')
