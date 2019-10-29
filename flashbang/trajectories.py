73e30bab6be4519ce07672735847a7aef111e916
Zac Johnston <zac.johnston@monash.edu>
2019-10-29 16:14:02 -0400

    [trajectories.py] added stir_traj_filepath()

--- flashbang/trajectories.py
+++ flashbang/trajectories.py
@@ -5,10 +5,10 @@ import numpy as np
 from . import load_save
 
 # TODO:
-#   - load snec profile array
 #   - extract mass grid from stir traj's
 #   2. build snec traj
 #       - For each mass, join profiles in time
+#       - subtract t=0 offset
 #   3. patch stir and snec
 #   4. save files
 
@@ -19,8 +19,8 @@ def load_stir_traj(tracer_i, basename='stir2_oct8_s12.0_alpha1.25',
     """Load STIR trajectory from file
         Returns: 2D np.array
     """
-    filename = f'{basename}{prefix}{tracer_i}{extension}'
-    filepath = os.path.join(path, filename)
+    filepath = stir_traj_filepath(tracer_i, basename=basename, prefix=prefix,
+                                  extension=extension, path=path)
     return np.loadtxt(filepath, skiprows=2)
 
 
@@ -32,13 +32,17 @@ def load_snec_profile(var, path='/Users/zac/projects/codes/traj_code/snec'):
     return np.load(filepath)
 
 
-def load_snec_time_grid():
-    """Load time grid from file
+def extract_stir_mass_grid(n_traj):
+    """Obtain mass grid from stir trajectory file headers
     """
     pass
 
 
-def load_snec_mass_grid():
-    """Load snec mass grid from file
+def stir_traj_filepath(tracer_i, basename='stir2_oct8_s12.0_alpha1.25',
+                       prefix='_tracer', extension='.dat',
+                       path='/Users/zac/projects/codes/traj_code/traj_s12.0_1024'):
+    """Returns formatted filepath to stir trajectory file
     """
-    pass
+    filename = f'{basename}{prefix}{tracer_i}{extension}'
+    return os.path.join(path, filename)
+
