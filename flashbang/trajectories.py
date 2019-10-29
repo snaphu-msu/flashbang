1bd074c225206a9721fe881a82285558246f3889
Zac Johnston <zac.johnston@monash.edu>
2019-10-29 15:45:22 -0400

    [trajectories.py] created module for calclating mass trajectories

---
+++ flashbang/trajectories.py
@@ -0,0 +1,32 @@
+from . import load_save
+
+# TODO:
+#   1. load stir traj file
+#   2. build snec traj
+#       - For each time, join mass profiles
+#   3. patch stir and snec
+#   4. save files
+
+
+def load_traj(path='/Users/zac/projects/codes/traj_code/traj_s12.0_1024'):
+    """Load STIR trajectory from file
+    """
+    pass
+
+
+def load_snec_profile(path='/Users/zac/projects/codes/traj_code/snec'):
+    """Load precomputed SNEC profiles from file
+    """
+    pass
+
+
+def load_snec_time_grid():
+    """Load time grid from file
+    """
+    pass
+
+
+def load_snec_mass_grid():
+    """Load snec mass grid from file
+    """
+    pass
