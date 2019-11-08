886546e4b1659f4154efbe82d1e23023a5ce013a
Zac Johnston <zac.johnston@monash.edu>
2019-11-08 18:13:22 -0500

    [plot_tools.py] added arg sub_figsize to setup_subplots()

--- flashbang/plot_tools.py
+++ flashbang/plot_tools.py
@@ -7,7 +7,7 @@ General functions for plotting
 """
 
 
-def setup_subplots(n_sub, max_cols=2, **kwargs):
+def setup_subplots(n_sub, max_cols=2, sub_figsize=(6, 5), **kwargs):
     """Constructs fig for given number of subplots
 
     returns : fig, ax
@@ -18,11 +18,13 @@ def setup_subplots(n_sub, max_cols=2, **kwargs):
         number of subplots (axes)
     max_cols : int
         maximum number of columns to arange subplots
+    sub_figsize : tuple
+        figsize of each subplot
     **kwargs :
         args to be parsed to plt.subplots()
     """
     n_rows = int(np.ceil(n_sub / max_cols))
     n_cols = {False: 1, True: max_cols}.get(n_sub > 1)
-
-    return plt.subplots(n_rows, n_cols, **kwargs)
+    figsize = (n_cols*sub_figsize[0], n_rows*sub_figsize[1])
+    return plt.subplots(n_rows, n_cols, figsize=figsize, **kwargs)
 
