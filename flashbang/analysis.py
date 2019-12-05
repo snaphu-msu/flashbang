f63e4f0f189605983488b27b480ac86dbaa21ba5
Zac Johnston <zac.johnston@monash.edu>
2019-12-04 19:36:15 -0500

    [analysis.py] added extract_multi_mass_tracers()

--- flashbang/analysis.py
+++ flashbang/analysis.py
@@ -1,16 +1,48 @@
 import numpy as np
-import sys
 from scipy.interpolate import interp1d
 import astropy.units as units
+
+# flashbang
+from .strings import printv
 
 g_to_msun = units.g.to(units.M_sun)
 
 
-def extract_all_mass_tracers():
-    """
+def extract_multi_mass_tracers(mass_grid, profiles, params, verbose=True):
+    """Iterate over chk profiles and interpolate mass shell tracers for each
 
+    Returns: np.ndarray
+        3D array of shape [n_profiles, n_tracers, n_params],
+        where n_profiles=len(profiles), n_tracers=len(mass_grid) and n_params=len(params).
+
+    parameters
+    ----------
+    mass_grid : [float]
+        1D array of mass shells to track.
+    profiles : {pd.DataFrame}
+        set of profile tables for each chk, with columns of params (including mass shells)
+        and rows of radial zones (see: load_save.extract_profile).
+    params : [str]
+        list of profile quantities to extract.
+    verbose : bool
     """
-    pass
+    printv(f'Extracting mass tracers from chk profiles', verbose=verbose)
+
+    n_profiles = len(profiles)
+    n_tracers = len(mass_grid)
+    n_params = len(params)
+
+    data_cube = np.zeros([n_profiles, n_tracers, n_params])
+
+    for i, chk in enumerate(profiles.keys()):
+        printv(f'\rchk: {chk} ({100 * i / n_profiles:.0f}%)', verbose, end='')
+        profile = profiles[i]
+
+        data_cube[i, :, :] = extract_mass_tracers(mass_grid=mass_grid,
+                                                  profile=profile,
+                                                  params=params)
+    printv('\n', verbose)
+    return data_cube
 
 
 def extract_mass_tracers(mass_grid, profile, params):
