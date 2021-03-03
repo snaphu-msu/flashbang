7cb04fe07bf055b18760ba7033c00b5960b92a82
Zac Johnston <zacpjohnston@gmail.com>
2021-03-03 13:02:12 -0500

    [plotter.py] fixed check_config()

--- flashbang/plotter/plotter.py
+++ flashbang/plotter/plotter.py
@@ -12,22 +12,23 @@ class Plotter:
     def __init__(self,
                  ax=None,
                  config=None,
-                 set_all=True,
                  chk=None,
                  bounce_time=0,
-                 y_var=None, x_var=None,
-                 y_scale=None, x_scale=None,
+                 x_var=None, y_var=None,
+                 x_scale=None, y_scale=None,
                  x_label=None, y_label=None,
                  x_lims=None, y_lims=None,
                  legend=False, legend_loc=None,
                  title=False, title_str=None,
-                 figsize=None):
+                 figsize=None,
+                 set_all=False,
+                 verbose=True,
+                 ):
         """
         parameters
         ----------
         ax : pyplot Axis
         config : str or Config
-        set_all : bool
         chk : int
         y_var : str
         x_var : str
@@ -42,13 +43,15 @@ class Plotter:
         title : bool
         title_str : str
         figsize : []
+        set_all : bool
+        verbose : bool
         """
         self.chk = chk
         self.bounce_time = bounce_time
-        self.y_var = y_var
         self.x_var = x_var
-        self.y_scale = y_scale
+        self.y_var = y_var
         self.x_scale = x_scale
+        self.y_scale = y_scale
         self.x_label = x_label
         self.y_label = y_label
         self.x_lims = x_lims
@@ -58,9 +61,11 @@ class Plotter:
         self.title = title
         self.title_str = title_str
         self.figsize = figsize
+        self.verbose = verbose
 
         self.fig, self.ax = self.check_ax(ax=ax)
         self.config = self.check_config(config=config)
+        self.check_properties()
 
         if set_all:
             self.set_all()
@@ -83,8 +88,8 @@ class Plotter:
 
         Returns : Config
         """
-        if type(config) in [str, None]:
-            return Config(name=config)
+        if (type(config) is str) or (config is None):
+            return Config(name=config, verbose=self.verbose)
         elif type(config) is Config:
             return config
         else:
