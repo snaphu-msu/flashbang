24eb6627d3ed7180ca0742845ff4f5146e8b55ab
Zac Johnston <zacpjohnston@gmail.com>
2023-02-03 15:30:01 -0500

    [__init__.py] load Simulation and Comparison objects directly

--- flashbang/__init__.py
+++ flashbang/__init__.py
@@ -1,13 +1,14 @@
-from . import simulation
 from . import load_save
 from . import paths
 from . import quantities
 from . import tools
-from . import comparison
 from . import plotting
 
-__all__ = ['simulation',
-           'comparison',
+from .simulation import Simulation
+from .comparison import Comparison
+
+__all__ = ['Simulation',
+           'Comparison',
            'load_save',
            'paths',
            'plotting',
